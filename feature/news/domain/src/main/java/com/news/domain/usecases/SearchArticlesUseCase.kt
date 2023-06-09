b0c05818635c8b78cb91654b61eb336130da723f
gustavo_dev <gustavo.isaac@gmail.com>
2023-06-09 09:06:29 +0100

    Resource

--- feature/news/domain/src/main/java/com/news/domain/usecases/SearchArticlesUseCase.kt
+++ feature/news/domain/src/main/java/com/news/domain/usecases/SearchArticlesUseCase.kt
@@ -1,12 +1,21 @@
 package com.news.domain.usecases
 
+import com.news.domain.Resource
+import com.news.domain.models.Article
 import com.news.domain.repositories.NewsRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class SearchArticlesUseCase /*@Inject*/ constructor(
     private val newsRepository: NewsRepository,
 ) {
 
-    suspend operator fun invoke(
-        map: MutableMap<String, String>,
-    ) = newsRepository.searchArticles(map)
+    suspend operator fun invoke(map: MutableMap<String, String>): Flow<Resource<List<Article>>> = flow {
+        emit(Resource.Loading())
+        try {
+            emit(Resource.Success(newsRepository.searchArticles(map)))
+        } catch (e: Exception) {
+            emit(Resource.Error(e.message.toString()))
+        }
+    }
 }
