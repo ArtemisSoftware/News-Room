6c6c09535af3665142738547cd0962e6825d8cbd
gustavo_dev <gustavo.isaac@gmail.com>
2023-06-12 17:14:48 +0100

    NewsHeader

---
+++ feature/news/presentation/src/main/java/com/news/presentation/news/composables/NewsHeader.kt
@@ -0,0 +1,51 @@
+package com.news.presentation.news.composables
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun NewsHeader(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+    ) {
+        Text(
+            text = "Today's date",
+            style = MaterialTheme.typography.titleMedium,
+            fontSize = 16.sp,
+            color = Color.Gray,
+        )
+
+        Text(
+            text = "Task-based \nExplanation Process",
+            modifier = Modifier.padding(bottom = 20.dp),
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            style = MaterialTheme.typography.headlineMedium,
+        )
+
+        //BottomNavNoAnimation(listOf(Screen.Home, Screen.Settings, Screen.Profile))
+        TopicSelector(listOf(Screen.Home, Screen.Settings, Screen.Profile))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun NewsHeaderPreview() {
+    NewsHeader(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+    )
+}
