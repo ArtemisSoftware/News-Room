d4824b3761566b11ab8e1bb22da6f999efcc4e86
gustavo <gustavo.isaac@gmail.com>
2023-07-07 07:32:03 +0100

    ReelsDto

---
+++ feature/reels/data/src/main/java/com/reels/data/ReelDto.kt
@@ -0,0 +1,22 @@
+package com.reels.data
+
+data class ReelDto(
+    val reelUrl: String,
+    val isFollowed: Boolean,
+    val reelInfo: ReelInfoDto,
+)
+
+data class ReelInfoDto(
+//    val username: String,
+//    val profilePicUrl: String,
+//    val description: String? = null,
+    val isLiked: Boolean,
+//    val likes: Int,
+//    val comments: Int,
+//    val audio: String = "$username • Original Audio",
+//    val audioPicUrl: String = profilePicUrl,
+//    val filter: String? = null,
+//    val location: String? = null,
+//    val taggedPeople: List<String>? = null,
+//    val id: String = UUID.randomUUID().toString()
+)
