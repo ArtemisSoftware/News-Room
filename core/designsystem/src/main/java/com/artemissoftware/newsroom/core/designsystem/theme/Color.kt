015cfc4cdb77852fae8b2e390392885dfdbde2ed
gustavo <gustavo.isaac@gmail.com>
2024-02-03 20:54:07 +0000

    Colors

---
+++ core/designsystem/src/main/java/com/artemissoftware/newsroom/core/designsystem/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.artemissoftware.newsroom.core.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+internal val White = Color(0xFFFFFFFF)
+internal val Black = Color(0xFF1C1E21) // Dark Background
+internal val Blue = Color(0xFF1877F2) // Primary
+internal val DarkRed = Color(0xFFC30052) // Dark Error
+internal val LightRed = Color(0xFFFF84B7)
+internal val LightBlack = Color(0xFF3A3B3C) // Dark Surface
+
+val BlueGray = Color(0xFFA0A3BD)
+internal val WhiteGray = Color(0xFFB0B3B8)
