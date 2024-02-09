f0a6f50d0c385724fbe87b99079cb970d204b2c3
gustavo <gustavo.isaac@gmail.com>
2024-02-09 06:27:53 +0000

    PreviewData

---
+++ feature/onboarding/src/main/java/com/artemissoftware/feature/onboarding/PreviewData.kt
@@ -0,0 +1,21 @@
+package com.artemissoftware.feature.onboarding
+
+import com.artemissoftware.newsroom.core.model.Page
+
+internal object PreviewData {
+
+    val pages = listOf(
+        Page(
+            title = "Lorem Ipsum is simply dummy",
+            description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        ),
+        Page(
+            title = "Lorem Ipsum is simply dummy",
+            description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        ),
+        Page(
+            title = "Lorem Ipsum is simply dummy",
+            description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        ),
+    )
+}
