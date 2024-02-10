f19a563a7a566220717ae15bb255d936687fe557
gustavo <gustavo.isaac@gmail.com>
2024-02-10 19:51:08 +0000

    OnBoardingViewModel

--- feature/onboarding/src/main/java/com/artemissoftware/feature/onboarding/OnBoardingScreen.kt
+++ feature/onboarding/src/main/java/com/artemissoftware/feature/onboarding/OnBoardingScreen.kt
@@ -1,93 +1,120 @@
 package com.artemissoftware.feature.onboarding
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.artemissoftware.feature.onboarding.composables.OnBoardingPage
+import com.artemissoftware.feature.onboarding.composables.PagerIndicator
 import com.artemissoftware.newsroom.core.designsystem.ThemePreviews
+import com.artemissoftware.newsroom.core.designsystem.composables.buttons.NRButton
+import com.artemissoftware.newsroom.core.designsystem.composables.buttons.NRTextButton
+import com.artemissoftware.newsroom.core.designsystem.theme.NewsRoomTheme
+import com.artemissoftware.newsroom.core.designsystem.theme.spacing
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun OnBoardingScreenContent(
-    // --onEvent: (OnBoardingEvent) -> Unit
+fun OnBoardingScreen(viewModel: OnBoardingViewModel = hiltViewModel()) {
+    val state = viewModel.state.collectAsState().value
+
+    OnBoardingScreenContent(
+        state = state,
+        event = viewModel::onTriggerEvent,
+    )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+private fun OnBoardingScreenContent(
+    state: OnBoardingState,
+    event: (OnBoardingEvent) -> Unit,
 ) {
+    val scope = rememberCoroutineScope()
+    val pagerState = rememberPagerState(initialPage = 0)
+
     Column(
         modifier = Modifier.fillMaxSize(),
     ) {
-//        val pagerState = rememberPagerState(initialPage = 0) {
-//            pages.size
-//        }
-//        val buttonsState = remember {
-//            derivedStateOf {
-//                when (pagerState.currentPage) {
-//                    0 -> listOf("", "Next")
-//                    1 -> listOf("Back", "Next")
-//                    2 -> listOf("Back", "Get Started")
-//                    else -> listOf("", "")
-//                }
-//            }
-//        }
-//        HorizontalPager(state = pagerState) { index ->
-//            OnBoardingPage(page = pages[index])
-//        }
-//        Spacer(modifier = Modifier.weight(1f))
+        HorizontalPager(
+            state = pagerState,
+            pageCount = state.pages.size,
+        ) { index ->
+            OnBoardingPage(
+                page = state.pages[index],
+                image = state.image,
+            )
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
         Row(
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .padding(horizontal = MediumPadding2)
-//                .navigationBarsPadding(),
-//            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = MaterialTheme.spacing.spacing4)
+                .navigationBarsPadding(),
+            horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically,
         ) {
-//            PagerIndicator(
-//                modifier = Modifier.width(52.dp),
-//                pagesSize = pages.size,
-//                selectedPage = pagerState.currentPage
-//            )
-//
+            PagerIndicator(
+                modifier = Modifier,
+                pagesSize = state.pages.size,
+                selectedPage = pagerState.currentPage,
+            )
+
             Row(verticalAlignment = Alignment.CenterVertically) {
-//                val scope = rememberCoroutineScope()
-//                //Hide the button when the first element of the list is empty
-//                if (buttonsState.value[0].isNotEmpty()) {
-//                    NewsTextButton(
-//                        text = buttonsState.value[0],
-//                        onClick = {
-//                            scope.launch {
-//                                pagerState.animateScrollToPage(
-//                                    page = pagerState.currentPage - 1
-//                                )
-//                            }
-//
-//                        }
-//                    )
-//                }
-//                NewsButton(
-//                    text = buttonsState.value[1],
-//                    onClick = {
-//                        scope.launch {
-//                            if (pagerState.currentPage == 2) {
-//                                onEvent(OnBoardingEvent.SaveAppEntry)
-//                            } else {
-//                                pagerState.animateScrollToPage(
-//                                    page = pagerState.currentPage + 1
-//                                )
-//                            }
-//                        }
-//                    }
-//                )
+                if (state.previewsText.isNotEmpty()) {
+                    NRTextButton(
+                        text = state.previewsText,
+                        onClick = {
+                            scope.launch {
+                                pagerState.animateScrollToPage(
+                                    page = pagerState.currentPage - 1,
+                                )
+                                event(OnBoardingEvent.GoToPreviewsPage)
+                            }
+                        },
+                    )
+                }
+                NRButton(
+                    text = state.nextText,
+                    onClick = {
+                        scope.launch {
+                            pagerState.animateScrollToPage(
+                                page = pagerState.currentPage + 1,
+                            )
+                            event(OnBoardingEvent.GoToNextPage)
+                        }
+                    },
+                )
             }
         }
-//        Spacer(modifier = Modifier.weight(0.5f))
+
+        Spacer(modifier = Modifier.weight(0.5f))
     }
 }
 
 @ThemePreviews
 @Composable
 private fun OnBoardingScreenContentPreview() {
-    // com.artemissoftware.newsroom.core.designsystem.theme.NewsRoomTheme {
-    OnBoardingScreenContent()
-    // }
+    NewsRoomTheme {
+        OnBoardingScreenContent(
+            state = PreviewData.onBoardingState,
+            event = {},
+        )
+    }
 }
---
+++ feature/onboarding/src/main/java/com/artemissoftware/feature/onboarding/OnBoardingViewModel.kt
@@ -0,0 +1,59 @@
+package com.artemissoftware.feature.onboarding
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class OnBoardingViewModel @Inject constructor() : ViewModel() {
+
+    private val _state = MutableStateFlow(OnBoardingState())
+    val state: StateFlow<OnBoardingState> = _state.asStateFlow()
+
+    init {
+    }
+
+    fun onTriggerEvent(event: OnBoardingEvent) {
+        when (event) {
+            OnBoardingEvent.GoToNextPage -> updateNextPage()
+            OnBoardingEvent.GoToPreviewsPage -> TODO()
+        }
+    }
+
+    private fun updateNextPage() = with(_state) {
+        update {
+            it.copy(currentPage = (it.currentPage + 1))
+        }
+
+        if (value.reachedLastPage()) {
+        } else {
+            updateTexts()
+        }
+    }
+
+    private fun updatePreviewsPage() = with(_state) {
+        update {
+            it.copy(currentPage = (it.currentPage - 1))
+        }
+        updateTexts()
+    }
+
+    private fun updateTexts() = with(_state) {
+        val (previews, next) = when (value.currentPage) {
+            0 -> "" to "Next"
+            1 -> "Back" to "Next"
+            2 -> "Back" to "Get Started"
+            else -> "" to ""
+        }
+        update {
+            it.copy(
+                previewsText = previews,
+                nextText = next,
+            )
+        }
+    }
+}
