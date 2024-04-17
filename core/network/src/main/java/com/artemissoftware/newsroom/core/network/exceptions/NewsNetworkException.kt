66bb51b9786483681187ee0a5b25a08a0b254a14
gustavo <gustavo.isaac@gmail.com>
2024-04-17 22:46:18 +0100

    Handle errors - part 3

--- core/network/src/main/java/com/artemissoftware/newsroom/core/network/exceptions/NewsNetworkException.kt
+++ core/network/src/main/java/com/artemissoftware/newsroom/core/network/exceptions/NewsNetworkException.kt
@@ -1,6 +1,9 @@
 package com.artemissoftware.newsroom.core.network.exceptions
 
+import com.artemissoftware.newsroom.core.network.NetworkError
+
 data class NewsNetworkException(
     val code: Int = -1,
     val description: String = "Unknown",
+    val loo: NetworkError = NetworkError.SocketTimeout
 ) : RuntimeException()
