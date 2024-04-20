27ef5b9a8e2cea12a39a2bd4dca7ffa1778adc6a
gustavo <gustavo.isaac@gmail.com>
2024-04-20 16:08:40 +0100

    Modifier shimmer

---
+++ core/ui/src/main/java/com/core/ui/util/extensions/ModifierExtension.kt
@@ -0,0 +1,28 @@
+package com.core.ui.util.extensions
+
+import android.annotation.SuppressLint
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import com.artemissoftware.newsroom.core.designsystem.theme.palette
+
+@SuppressLint("ModifierFactoryUnreferencedReceiver")
+fun Modifier.shimmerEffect() = composed {
+    val transition = rememberInfiniteTransition(label = "")
+    val alpha = transition.animateFloat(
+        initialValue = 0.2f,
+        targetValue = 0.9f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 1000),
+            repeatMode = RepeatMode.Reverse,
+        ),
+        label = "",
+    ).value
+    background(color = MaterialTheme.palette.extraColors.shimmer.copy(alpha = alpha))
+}
